4a29210b90aed182fb403df5539a77b4932303a6
habibur5313 <habiburmamun313@gmail.com>
2024-10-06 04:06:28 +0600

    second-commit

--- js/script.js
+++ js/script.js
@@ -10,16 +10,31 @@ const loadBtn = () => {
 const displayBtn =(buttons) => {
                     const buttonContainer = document.getElementById('button-container')
                     buttons.forEach((item) => {
+                                        
                                         const div = document.createElement('div')
                                         div.innerHTML= `
-                                        <div class= "flex gap-5 px-20 py-5 w-full border rounded-xl ">
+                                        <div  onclick="displayOneBtn('${item.category}')" class= "flex gap-5 px-20 py-5 w-full border rounded-xl ">
                                         <img src=${item.category_icon}/>
-                                        <button class="text-3xl font-semibold">${item.category}</button>
+                                        <button  class="text-3xl font-semibold">${item.category}</button>
                                         </div>
                                         `
                                         buttonContainer.appendChild(div)
                                         
                     });
+                    
+}
+
+const loadCategoryVideos = (category) => {
+                    fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+                    .then(res => res.json())
+                    .then(data => displayVideos(data.data)
+                    )
+                    .catch(err => console.error(err))
+                    
+}
+
+const displayOneBtn = (category) => {
+                    loadCategoryVideos(category)
                     
 }
 
@@ -37,23 +52,37 @@ const displayBtn =(buttons) => {
 //                 }
 
                     //                   *DISPLAY VIDEOS*               //
-const loadVideos = async() => {
-                 fetch('https://openapi.programming-hero.com/api/peddy/pets')
+const loadVideos = (category) => {
+                 fetch(`https://openapi.programming-hero.com/api/peddy/pets?category/${category}`)
                  .then(res => res.json())
                  .then(data => displayVideos(data.pets)
                  )
                  .catch(err => console.error(err))
+                 console.log(category);
+                 
 }
 
 const displayVideos = (videos) => {
                     const cardContainer = document.getElementById('card-container')
+                    cardContainer.innerHTML = ''
+                    if(videos.length === 0){
+                                        cardContainer.classList.remove('grid')
+                                       cardContainer.innerHTML = `
+                                         <div class = "flex flex-col h-[400px] justify-center items-center gap-4">
+                                         <img src="images/error.webp" alt="">
+                                         <h1 class= "text-2xl font-bold "> No Content Here IN This Category</h1>
+                                         </div>
+                                        `}
+                                        else{
+                                                            cardContainer.classList.add('grid')        
+                                        }
               videos.forEach(item => {
 
                     const div = document.createElement('div')
                     div.innerHTML = `
                     <div class="card card-compact object-cover border">
                                                                                                     <figure class= "mt-2 h-[200px]">
-                                                                                                      <img class= "h-full rounded-xl"
+                                                                                                      <img class= "h-full px-2 rounded-xl"
                                                                                                         src=${item.image}/>
                                                                                                     </figure>
                                                                                                     <div class="card-body">
