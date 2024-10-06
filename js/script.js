813b669d37ef3916b70fdfa234ce9de0d41b2c2d
habibur5313 <habiburmamun313@gmail.com>
2024-10-07 01:55:58 +0600

    forth-commit

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const displayBtn =(buttons) => {
                                         
                                         const div = document.createElement('div')
                                 div.innerHTML= `
-                                        <div id= "btn-${item.category}" onclick="displayOneBtn('${item.category}')" class= "category-btn flex gap-5 px-20 py-5 w-full border rounded-xl ">
+                                        <div id= "btn-${item.category}" onclick="displayOneBtn('${item.category}')" class= "category-btn flex justify-center gap-5  py-5 w-full border rounded-xl ">
                                         <img src=${item.category_icon}/>
                                         <button  class="text-3xl font-semibold">${item.category}</button>
                                         </div>
@@ -25,6 +25,7 @@ const displayBtn =(buttons) => {
 }
 
 const loadCategoryVideos = (category) => {
+  document.getElementById('api-card').classList.remove('hidden')
   document.getElementById('spinner').classList.add('hidden')
                     fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
                     .then(res => res.json())
@@ -44,6 +45,7 @@ const loadCategoryVideos = (category) => {
 }
 
 const displayOneBtn = (category) => {
+  document.getElementById('api-card').classList.add('hidden')
   document.getElementById('spinner').classList.remove('hidden')
                     setTimeout(() => {
                       loadCategoryVideos(category)
@@ -66,6 +68,7 @@ const displayOneBtn = (category) => {
 
                     //                   *DISPLAY VIDEOS*               //
 const loadVideos = (category) => {
+  document.getElementById('api-card').classList.remove('hidden')
   document.getElementById('spinner').classList.add('hidden')
                  fetch(`https://openapi.programming-hero.com/api/peddy/pets?category/${category}`)
                  .then(res => res.json())
@@ -83,7 +86,7 @@ const displayVideos = (videos) => {
                     if(videos.length === 0){
                             cardContainer.classList.remove('grid')
                             cardContainer.innerHTML = `
-                              <div class = "flex flex-col h-[600px] justify-center items-center gap-4">
+                              <div class = "flex flex-col h-[400px] sm:h-[600px] justify-center items-center gap-4">
                               <img src="images/error.webp" alt="">
                               <h1 class= "text-2xl font-bold "> No Content Here IN This Category</h1>
                               </div>
@@ -96,18 +99,20 @@ const displayVideos = (videos) => {
                     const div = document.createElement('div')
                     div.innerHTML = `
                     <div class="card card-compact object-cover border">
-                        <figure class= "mt-2 h-[200px]">
-                          <img class= "h-full px-2 rounded-xl"
+                        <figure class= "mt-2 h-[250px] sm:h-[200px] ">
+                          <img class= "h-full px-2 w-full rounded-xl"
                             src=${item.image}/>
                         </figure>
                         <div class="card-body">
                           <h2 class="card-title">${item.pet_name}</h2>
-                          <p>Breed : ${item.breed ? item.breed : 'Not Available'}</p>
-                          <p>Birth : ${item.date_of_birth? item.date_of_birth:'Not Available'}</p>
-                          <p>Gender : ${item.gender?item.gender:'Not Available'}</p>
-                          <p>Price : ${item.price ? item.price: 'Not Available'}</p>
-                          <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                          <p class= "flex"><img src="images/Frame (2).png" alt=""> Breed : ${item.breed ? item.breed : 'Not Available'}</p>
+                          <p class= "flex" ><img src="images/Frame.png" alt=""> Birth : ${item.date_of_birth? item.date_of_birth:'Not Available'}</p>
+                          <p class= "flex"> <img src="images/Frame (1).png" alt=""> Gender : ${item.gender?item.gender:'Not Available'}</p>
+                          <p class= "flex"><img src="images/Frame (3).png" alt=""> Price : ${item.price ? item.price: 'Not Available'}</p>
+                          <div class=" flex justify-between gap-4">
+                            <button  onclick="addImg('${item.image}')" class = "flex justify-center py-2 w-full border rounded-xl"><img src="images/Vector.png" alt=""></button>
+                            <button class = "w-full py-2 border rounded-xl text-green-700">Adopt</button>
+                            <button onclick="detailsModal('${item.image}','${item.pet_name}','${item.breed}','${item.date_of_birth}','${item.gender}','${item.price}')" class = " w-full py-2 border rounded-xl text-green-700">Details</button>
                           </div>
                         </div>
                       </div>
@@ -118,7 +123,38 @@ const displayVideos = (videos) => {
               
 }
 
+const detailsModal = (one, two, three, four, five, six) => {
+  const modalContainer = document.getElementById("details-modal-div")
+       modalContainer.innerHTML = `
+                                <img class= "w-full rounded-xl" src = ${one}/> 
+                                <div class="card-body">
+                                <h2 class="card-title">${two}</h2>
+                          <p class= "flex"><img src="images/Frame (2).png" alt=""> Breed : ${three ? three : 'Not Available'}</p>
+                          <p class= "flex" ><img src="images/Frame.png" alt=""> Birth : ${four ? four:'Not Available'}</p>
+                          <p class= "flex"> <img src="images/Frame (1).png" alt=""> Gender : ${five ? five :'Not Available'}</p>
+                          <p class= "flex"><img src="images/Frame (3).png" alt=""> Price : ${six ? six : 'Not Available'}</p>
+                          </div>
+       `
+
+
+        // document.getElementById('showModalData').click()
+        document.getElementById('my_modal_5').showModal()
+  
+}
+
+const addImg = (img) => {
+  const cardImgAdd = document.getElementById('card-image-add')
+  const div = document.createElement('div')
+  div.innerHTML = `<figure class= "mt-2">
+                            <img class= " rounded-xl" src = ${img}/> 
+                            </figure>
+  `
+  cardImgAdd.appendChild(div)
+  
+}
+
 const loadVideosCall = () => {
+  document.getElementById('api-card').classList.add('hidden')
   document.getElementById('spinner').classList.remove('hidden')
   setTimeout(() => {
     loadVideos()
